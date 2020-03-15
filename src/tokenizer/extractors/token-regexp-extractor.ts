5402daf49f625bd44decfade685fec5eb7aac4e6
Andrey Korovin <misticwonder@gmail.com>
2020-03-15 12:33:06 +0200

    remove string extractor

--- src/tokenizer/extractors/token-regexp-extractor.ts
+++ src/tokenizer/extractors/token-regexp-extractor.ts
@@ -2,7 +2,15 @@ import { Token } from '../entities/token';
 import { TokenExtractor } from '../entities/token-extractor';
 
 export class TokenRegexpExtractor<Type> implements TokenExtractor<Type> {
-  constructor(private type: Type, private regexp: RegExp) {}
+  private regexp: RegExp;
+
+  constructor(private type: Type, regexp: RegExp | string) {
+    if (!regexp) {
+      throw new Error('RegExp should not be empty');
+    }
+
+    this.regexp = new RegExp(regexp);
+  }
 
   extract(input: string, index: number): Token<Type> | null {
     const result: RegExpExecArray | null = this.regexp.exec(input.slice(index));

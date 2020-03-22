e05a255f28af6542413c36b0feb9383199a76cf1
Andrey Korovin <misticwonder@gmail.com>
2020-03-22 16:18:43 +0200

    index back

--- src/tokenizer/extractors/token-regexp-extractor.ts
+++ src/tokenizer/extractors/token-regexp-extractor.ts
@@ -5,18 +5,19 @@ export class TokenRegexpExtractor<Type> implements TokenExtractor<Type> {
   constructor(
     private type: Type,
     private regexp: RegExp,
-    private matchIndex = 0,
+    private matchGroup = 0,
+    private indexBack = 0,
     private charsBack = 0,
     private postProcessValue = (value: string): string => value,
   ) {}
 
   extract(input: string, index: number): Token<Type> | null {
-    const result: RegExpExecArray | null = this.regexp.exec(input.slice(index));
+    const result: RegExpExecArray | null = this.regexp.exec(input.slice(index - this.indexBack));
 
     if (result !== null && result.index === 0) {
       return {
         type: this.type,
-        value: this.postProcessValue(result[this.matchIndex]),
+        value: this.postProcessValue(result[this.matchGroup]),
         origin: result[0],
         charsBack: this.charsBack,
       };

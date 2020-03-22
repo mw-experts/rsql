635cb9719e641cd0ae9756f5913cf0d7b7558593
Andrey Korovin <misticwonder@gmail.com>
2020-03-22 16:19:21 +0200

    token types

--- src/rsql-tokenizer/rsql-token-type.ts
+++ src/rsql-tokenizer/rsql-token-type.ts
@@ -1,7 +1,21 @@
 export enum RsqlTokenType {
   Space = 'SPACE',
-  String = 'STRING',
-  Paren = 'PAREN',
-  BasicOperator = 'BASIC_OPERATOR',
-  CompositeOperator = 'COMPOSITE_OPERATOR',
+
+  CompositeAndOperator = 'COMPOSITE_AND_OPERATOR',
+  CompositeOrOperator = 'COMPOSITE_OR_OPERATOR',
+
+  BasicEqualOperator = 'BASIC_EQUAL_OPERATOR',
+  BasicNotEqualOperator = 'BASIC_NOT_EQUAL_OPERATOR',
+  BasicGreaterOperator = 'BASIC_GREATER_OPERATOR',
+  BasicGreaterOrEqualOperator = 'BASIC_GREATER_OR_EQUAL_OPERATOR',
+  BasicLessOperator = 'BASIC_LESS_OPERATOR',
+  BasicLessOrEqualOperator = 'BASIC_LESS_OR_EQUAL_OPERATOR',
+  BasicInOperator = 'BASIC_IN_OPERATOR',
+  BasicNotInOperator = 'BASIC_NOT_IN_OPERATOR',
+
+  ParenLeft = 'PAREN_LEFT',
+  ParenRight = 'PAREN_RIGHT',
+
+  Field = 'FIELD',
+  Value = 'VALUE',
 }

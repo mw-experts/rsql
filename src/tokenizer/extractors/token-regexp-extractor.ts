63860034349fae16a871eddb42936786fc63df9b
Andrey Korovin <misticwonder@gmail.com>
2020-03-22 13:58:23 +0200

    tokenizer update

--- src/tokenizer/extractors/token-regexp-extractor.ts
+++ src/tokenizer/extractors/token-regexp-extractor.ts
@@ -2,15 +2,12 @@ import { Token } from '../entities/token';
 import { TokenExtractor } from '../entities/token-extractor';
 
 export class TokenRegexpExtractor<Type> implements TokenExtractor<Type> {
-  private regexp: RegExp;
-
-  constructor(private type: Type, regexp: RegExp | string, private matchIndex = 0) {
-    if (!regexp) {
-      throw new Error('RegExp should not be empty');
-    }
-
-    this.regexp = typeof regexp === 'string' ? new RegExp(regexp) : regexp;
-  }
+  constructor(
+    private type: Type,
+    private regexp: RegExp,
+    private matchIndex = 0,
+    private postProcessValue = (value: string): string => value,
+  ) {}
 
   extract(input: string, index: number): Token<Type> | null {
     const result: RegExpExecArray | null = this.regexp.exec(input.slice(index));
@@ -18,7 +15,7 @@ export class TokenRegexpExtractor<Type> implements TokenExtractor<Type> {
     if (result !== null && result.index === 0) {
       return {
         type: this.type,
-        value: result[this.matchIndex],
+        value: this.postProcessValue(result[this.matchIndex]),
         origin: result[0],
       };
     }

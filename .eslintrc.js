b14b75828075d5c85e60f29380d2b62d14ecf194
Andrey Korovin <misticwonder@gmail.com>
2020-04-14 22:18:21 +0300

    refactor configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,15 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   plugins: [
     '@typescript-eslint',
     'jest',
-    'promise',
-    'unicorn',
-    'sonarjs',
-    'prettier',
   ],
   extends: [
     'airbnb-typescript/base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:jest/recommended',
-    'plugin:jest/style',
-    'plugin:promise/recommended',
-    'plugin:unicorn/recommended',
-    'plugin:sonarjs/recommended',
-    'plugin:prettier/recommended',
-    'prettier',
-    "prettier/@typescript-eslint",
-    "prettier/unicorn",
+    'plugin:jest/all',
   ],
-  rules: {
-    'no-console': 'error',
-    'no-continue': 'off',
-    'no-plusplus': 'off',
-    'no-useless-escape': 'off',
-    'class-methods-use-this': 'off',
-    'import/prefer-default-export': 'off',
-    '@typescript-eslint/prefer-string-starts-ends-with': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    'sonarjs/no-duplicate-string': 'off'
-  }
+  parserOptions: {
+    project: './tsconfig.json',
+  },
 };
--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,4 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  bail: 1,
-  errorOnDeprecated: true,
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: -10,
-    },
-  },
 };

7a866c9f6f3bc60c900f52bb14e33e4dc4bd4d60
Andrey Korovin <misticwonder@gmail.com>
2021-12-21 12:31:53 +0200

    feat(matcher): depp find in array notation

--- src/helpers/deep-find-property.ts
+++ src/helpers/deep-find-property.ts
@@ -1,8 +1,12 @@
 export function deepFindProperty<T>(object: object, path: string, separator = '.'): T | null {
-  return path.split(separator).reduce(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (result: any, key: string) =>
-      result !== null && result !== undefined && result[key] !== undefined ? result[key] : null,
-    object,
-  );
+  return path
+    .replace(/\[/g, separator)
+    .replace(/\]/g, '')
+    .split(separator)
+    .reduce(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (result: any, key: string) =>
+        result !== null && result !== undefined && result[key] !== undefined ? result[key] : null,
+      object,
+    );
 }
